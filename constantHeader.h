47ebdac665b26e9bcbee0e09b1f28c2b0a42c0b1
inves <alphonserivoire@gmail.com>
2023-11-27 09:06:44 +0100

    Initial commit

---
+++ constantHeader.h
@@ -0,0 +1,12 @@
+//
+// Created by inves on 27/11/2023.
+//
+
+#ifndef UNTITLED_CONSTANTHEADER_H
+#define UNTITLED_CONSTANTHEADER_H
+
+#define WELCOME_MSG "Welcome to ENSEA Tiny Shell \n Type 'exit' to quit."
+
+#endif //UNTITLED_CONSTANTHEADER_H
+
+
